5911b4f1ff197ecafcc82e23219324b364454ac5
na57 <na57@qq.com>
2016-11-24 09:32:14 +0800

    remove top buttons && add footer

--- src/routes/list/List.js
+++ src/routes/list/List.js
@@ -6,6 +6,7 @@ import Filter from './Filter';
 import ResItem from './ResItem';
 import * as actions from '../../actions/list';
 import EnsureSingup from '../common/EnsureSignupWxe';
+import Footer from '../common/Footer';
 
 class List extends React.Component {
   render() {
@@ -17,28 +18,21 @@ class List extends React.Component {
     return (
       <Container>
         <EnsureSingup />
-        <div className="weui-flex">
-            <div className="weui-flex__item">
-            </div>
-            <div className="weui-flex__item" style={{ textAlign: 'right' }}>
-              <Button style={{ margin: '5px' }} type="default" size="small" plain href="/add" >
-                新增资源 &gt;&gt;
-              </Button>
-            </div>
-        </div>
         <SearchBar onChange={search.bind(this)} />
         <CellsTitle>筛选</CellsTitle>
         <Filter />
         <CellsTitle>共{filteredResources.length}个资源</CellsTitle>
         <Cells>
           {
-            filteredResources.length ? filteredResources.map(res => <ResItem key={res._id} {...res} />)
+            filteredResources.length
+            ? filteredResources.map(res => <ResItem key={res._id} {...res} />)
             : (
             <div className="weui-loadmore weui-loadmore_line">
               <span className="weui-loadmore__tips">暂无数据</span>
             </div>)
           }
         </Cells>
+        <Footer />
         <Toast loading show={loading} >处理中</Toast>
       </Container>
     );
