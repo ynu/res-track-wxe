3bcf67c15df05caa1e25694741698a7e38d14913
na57 <na57@qq.com>
2016-12-05 15:00:03 +0800

    fix: 添加资源页面，将资源类别改为由数据库获取

--- src/routes/add/index.js
+++ src/routes/add/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { SUCCESS, OTHER_ERROR } from 'nagu-validates';
 import Add from './Add';
 import { addResource } from '../../actions/add';
+import fetch from '../../core/fetch';
+
+const getResCatagories = async () => {
+  try {
+    const res = await fetch('/api/res-catagory');
+    const result = await res.json();
+    if (result.ret === SUCCESS) return result.data;
+    throw result;
+  } catch (msg) {
+    throw { ret: OTHER_ERROR, msg };
+  }
+};
 
 export default {
 
@@ -15,9 +28,10 @@ export default {
         console.log(e);
       }
     };
+    const catagories = await getResCatagories();
     return {
       title: '添加资源',
-      component: <Add submitRes={add} />,
+      component: <Add submitRes={add} resCatagories={catagories} />,
     };
   },
 };
