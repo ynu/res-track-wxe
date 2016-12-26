7edc3453bb8cdd46e73e3c9455d63d9224082392
na57 <na57@qq.com>
2016-12-26 13:55:19 +0800

    update test

--- src/routes/add-state/AddState.js
+++ src/routes/add-state/AddState.js
@@ -7,6 +7,7 @@ import PageHeader from '../detail/PageHeader';
 import EnsureSingup from '../common/EnsureSignupWxe';
 import * as actions from '../../actions/detail';
 import * as fileActions from '../../actions/files';
+import * as wechatActions from '../../actions/wechat';
 import Footer from '../common/Footer';
 import fetch from '../../core/fetch';
 
@@ -19,16 +20,13 @@ const evalWXjsApi = function (jsApiFun) {
   }
 };
 
-const selectUsers = () => null;
-
 class AddState extends React.Component {
   async componentDidMount() {
-    const { getResource, resId } = this.props;
+    const { getResource, resId, selectEnterpriseContact } = this.props;
     getResource(resId);
 
     const jsApiList = ['openEnterpriseContact'];
     const url = encodeURIComponent(window.location.href);
-    alert(url);
     let res = await fetch(`/api/wxe-auth/jsconfig?jsApiList=${JSON.stringify(jsApiList)}&url=${url}&debug=true`);
     const result = await res.json();
     wx.config(result.data);
@@ -37,28 +35,28 @@ class AddState extends React.Component {
       const result2 = await res.json();
       document.querySelector('#btnTest').onclick = function () {
         evalWXjsApi(() => {
-          alert(JSON.stringify(result2.data));
           WeixinJSBridge.invoke('openEnterpriseContact', {
             ...result2.data,
             params: {
               departmentIds: [0],    // 非必填，可选部门ID列表（如果ID为0，表示可选管理组权限下所有部门）
                       // 'tagIds' : [1],    // 非必填，可选标签ID列表（如果ID为0，表示可选所有标签）
                       // 'userIds' : ['zhangsan','lisi'],    // 非必填，可选用户ID列表
-              mode: 'single',    // 必填，选择模式，single表示单选，multi表示多选
-              type: ['department', 'tag', 'user'],    // 必填，选择限制类型，指定department、tag、user中的一个或者多个
+              mode: 'multi',    // 必填，选择模式，single表示单选，multi表示多选
+              type: ['user'],    // 必填，选择限制类型，指定department、tag、user中的一个或者多个
                       // 'selectedDepartmentIds' : [],    // 非必填，已选部门ID列表
                       // 'selectedTagIds' : [],    // 非必填，已选标签ID列表
                       // 'selectedUserIds' : [],    // 非必填，已选用户ID列表
             },
           }, (res) => {
-            alert(JSON.stringify(res));
             if (res.err_msg.indexOf('function_not_exist') > -1) {
               alert('版本过低请升级');
             } else if (res.err_msg.indexOf('openEnterpriseContact:fail') > -1) {
+              alert(JSON.stringify(res));
               return;
             }
+            alert(res.result);
             const result = JSON.parse(res.result);    // 返回字符串，开发者需自行调用JSON.parse解析
-            console.log(result);
+            selectEnterpriseContact(res.result);
                   // var selectAll = result.selectAll;     // 是否全选（如果是，其余结果不再填充）
                   // if (!selectAll)
                   // {
@@ -86,13 +84,32 @@ class AddState extends React.Component {
       };
     });
   }
+
+  renderSelectedUser() {
+    const { userList } = this.props.selectedEnterpriseContact || {};
+    if (!userList || !userList.length) return '请选择';
+
+    // const userList = [{
+    //   name: 'user1',
+    //   src: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAB/ElEQVR42u2VS6tBURTH7wcg7zcZMFNmBiiPSJkzEykzCUnJWBkqEwMiJgZKJgxMkKFXkpEPs25r1Tl53hzOde/g7Pp3ztlr7bV+7b32Ol9f/2WsViv4pAQAAeBpgH6/D06nE0KhEMxms88DpNNpEIvFIBKJoNlsfh5gOBxCIBCAWCwGy+XyraTr9ZrVr9bAeaJHehqgUqmAz+cDj8dzURculwvcbjfVRSKRYL9R8Xgc5vM5PwCZTAbkcjlIpVJ2DmtBIpGATCYjOHyiFAoFCX2j0Sg/ANlsFtRqNSiVSnau1WqBSqUCnU4HFosF6vU6DAYDUiQSAY1GQ9Dj8fh9gFwuR4kwKHO+7Xab5kwmE5RKJdjv97Db7Ui1Wg0MBgP5l8tlfgD0ej0FZBYjACZBgEajcRF4Op2C0Wgku9/v5w9Aq9XeAGCiTqdzE5yxBYNBfgAw4DUAQmGSfD5/EXgymbAAxWLxNYBzx0cAzBHY7XZYLBasLZVKkQ01Go24A1w7/lQDZrOZbDabDbxeLzgcDvJDWzgchu12yw3gnmOhUKCgeO3uAVSrVbBarXRN0Qd3KplMcu+EjxwPhwOcTifSdQ3gEfR6Pbp+TB/A/8XxeOQP4J663S7tCvYCBMA5pg9sNpvX/gVcADARsyv4zmUtLwB8SAD4fwDCEMZfjW9zZMwmOWHKlAAAAABJRU5ErkJggg==',
+    //   id: 'id1',
+    // }, {
+    //   name: 'us1',
+    //   src: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAB/ElEQVR42u2VS6tBURTH7wcg7zcZMFNmBiiPSJkzEykzCUnJWBkqEwMiJgZKJgxMkKFXkpEPs25r1Tl53hzOde/g7Pp3ztlr7bV+7b32Ol9f/2WsViv4pAQAAeBpgH6/D06nE0KhEMxms88DpNNpEIvFIBKJoNlsfh5gOBxCIBCAWCwGy+XyraTr9ZrVr9bAeaJHehqgUqmAz+cDj8dzURculwvcbjfVRSKRYL9R8Xgc5vM5PwCZTAbkcjlIpVJ2DmtBIpGATCYjOHyiFAoFCX2j0Sg/ANlsFtRqNSiVSnau1WqBSqUCnU4HFosF6vU6DAYDUiQSAY1GQ9Dj8fh9gFwuR4kwKHO+7Xab5kwmE5RKJdjv97Db7Ui1Wg0MBgP5l8tlfgD0ej0FZBYjACZBgEajcRF4Op2C0Wgku9/v5w9Aq9XeAGCiTqdzE5yxBYNBfgAw4DUAQmGSfD5/EXgymbAAxWLxNYBzx0cAzBHY7XZYLBasLZVKkQ01Go24A1w7/lQDZrOZbDabDbxeLzgcDvJDWzgchu12yw3gnmOhUKCgeO3uAVSrVbBarXRN0Qd3KplMcu+EjxwPhwOcTifSdQ3gEfR6Pbp+TB/A/8XxeOQP4J663S7tCvYCBMA5pg9sNpvX/gVcADARsyv4zmUtLwB8SAD4fwDCEMZfjW9zZMwmOWHKlAAAAABJRU5ErkJggg==',
+    //   id: 'id2',
+    // }];
+    return userList.map(user => (
+      <img src={user.photo} alt={user.name} key={user.id} />
+    ));
+  }
+
   render() {
     const removePhoto = file => {
       const removedFile = this.props.files.find(photo => photo.url === file.url);
       this.props.remove(removedFile.serverId);
     };
-    const { Container, Button, ButtonArea, CellsTitle, Toast, Uploader, Cells, Cell, CellBody } = weui;
-    const { handleSubmit, noteLength, addState, resource, loading } = this.props;
+    const { Container, Button, ButtonArea, CellsTitle, Toast, Uploader, Cells, Cell, CellBody, CellFooter } = weui;
+    const { handleSubmit, noteLength, addState, resource, loading, selectedEnterpriseContact } = this.props;
 
     const add = values => addState({
       ...values,
@@ -121,7 +138,15 @@ class AddState extends React.Component {
               </CellBody>
             </Cell>
           </Cells>
-          <Button id="btnTest">测试</Button>
+          <CellsTitle>通知他人</CellsTitle>
+          <Cells>
+            <Cell href="javascript:;" id="btnTest" access>
+              <CellBody>
+                { this.renderSelectedUser() }
+              </CellBody>
+              <CellFooter />
+            </Cell>
+          </Cells>
           <ButtonArea>
             <Button onClick={handleSubmit(add)}>确定</Button>
           </ButtonArea>
@@ -141,11 +166,13 @@ const mapStateToProps = state => {
     loading: state.toast.loading,
     resource: state.detail,
     files: state.uploader.files,
+    ...state.wechat,
   };
 };
 export default connect(mapStateToProps, {
   ...actions,
   ...fileActions,
+  ...wechatActions,
 })(reduxForm({
   form: 'resource',
   initialValues: {
