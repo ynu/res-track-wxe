221dce49bde42262953fc6afd3349d373bc303e6
na57 <na57@qq.com>
2016-12-28 15:51:45 +0800

    update test

--- src/actions/wechat.js
+++ src/actions/wechat.js
@@ -1,12 +1,12 @@
-export const SELECT_ENTERPRISE_CONTACT = 'wechat/SELECT_ENTERPRISE_CONTACT';
+export const SELECT_ENTERPRISE_CONTACT = 'wechat/jssdk/SELECT_ENTERPRISE_CONTACT';
 
 export const selectEnterpriseContact = result => ({
   type: SELECT_ENTERPRISE_CONTACT,
   result,
 });
 
-export const JSSDK_CONFIG = 'wechat/JSSDK_CONFIG';
-export const JSSDK_READY = 'wechat/JSSDK_READY';
+export const JSSDK_CONFIG = 'wechat/jssdk/JSSDK_CONFIG';
+export const JSSDK_READY = 'wechat/jssdk/JSSDK_READY';
 
 export const jssdkConfig = config => ({
   type: JSSDK_CONFIG,
@@ -15,4 +15,21 @@ export const jssdkConfig = config => ({
 
 export const jssdkReady = () => ({
   type: JSSDK_READY,
+});
+
+export const SHOW_CONFIRM = 'wechat/weui/SHOW_CONFIRM';
+export const HIDE_CONFIRM = 'wechat/weui/HIDE_CONFIRM';
+
+export const showConfirm = data => ({
+  type: SHOW_CONFIRM,
+  data,
+});
+export const hideConfirm = () => ({
+  type: HIDE_CONFIRM,
+});
+
+export const SHOW_GALLERY = 'wechat/weui/SHOW_GALLERY';
+export const showGallery = file => ({
+  type: SHOW_GALLERY,
+  file,
 });
