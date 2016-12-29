659c36ac0f7fab16f7014da9228e0deab390b0b3
na57 <na57@qq.com>
2016-12-29 16:30:54 +0800

    删除不要的注释

--- src/components/WeChat/WxSelectUser.js
+++ src/components/WeChat/WxSelectUser.js
@@ -68,29 +68,6 @@ class WxSelectUser extends React.Component {
           selectedUserIds: props.selectedUserIds,
         },
       }))(this.props), cb));
-    //   const invoke = WxSelectUser.getInvoker(
-    //     'openEnterpriseContact',
-    //     {
-    //       ...groupConfig,
-    //       params: {
-    //         departmentIds: [0],    // 非必填，可选部门ID列表（如果ID为0，表示可选管理组权限下所有部门）
-    //         mode,    // 必填，选择模式，single表示单选，multi表示多选
-    //         type: ['user'],    // 必填，选择限制类型，指定department、tag、user中的一个或者多个
-    //         selectedUserIds: this.props.selectedUserIds,
-    //       },
-    //     },
-    //     res => {
-    //       if (!res) return;
-    //       if (res.err_msg.indexOf('function_not_exist') > -1) {
-    //         alert('版本过低请升级');
-    //       } else if (res.err_msg.indexOf('openEnterpriseContact:fail') > -1) {
-    //         alert(JSON.stringify(res));
-    //         return;
-    //       }
-    //       const result = JSON.parse(res.result);    // 返回字符串，开发者需自行调用JSON.parse解析
-    //       onFinish(result);
-    //     }
-    //   );
       bind(invoke);
     });
   }
